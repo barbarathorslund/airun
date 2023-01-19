b280967bc09b34b619bd1fcc5154be5ba7271a15
barbarathorslund <barbarathorslund@hotmail.com>
2023-01-19 17:42:43 +0100

    Add fileUpload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import * as React from "react";
+import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import FileUpload from "./components/fileUpload";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack>
+          <FileUpload />
+        </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
---
+++ src/components/fileUpload.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Center, useColorModeValue, Icon } from "@chakra-ui/react";
+import { AiFillFileAdd } from "react-icons/ai";
+
+function FileUpload() {
+  const onDrop = useCallback((acceptedFiles: any) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { "image/png": [], "image/jpg": [] },
+    maxFiles: 4,
+  });
+
+  const dropText = "Drop or select files";
+
+  const activeBg = useColorModeValue("gray.100", "gray.600");
+  const borderColor = useColorModeValue(
+    isDragActive ? "teal.300" : "gray.300",
+    isDragActive ? "teal.500" : "gray.500"
+  );
+
+  return (
+    <Center
+      p={10}
+      cursor="pointer"
+      bg={isDragActive ? activeBg : "transparent"}
+      _hover={{ bg: activeBg }}
+      transition="background-color 0.2s ease"
+      borderRadius={4}
+      border="3px dashed"
+      borderColor={borderColor}
+      {...getRootProps()}
+    >
+      <input {...getInputProps()} />
+      <Icon as={AiFillFileAdd} mr={2} />
+      <p>{dropText}</p>
+    </Center>
+  );
+}
+
+export default FileUpload;
