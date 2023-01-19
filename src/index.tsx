daafbf6edee3e480482011f389b0a59172574671
barbarathorslund <barbarathorslund@hotmail.com>
2023-01-19 16:10:52 +0100

    Setup initial project

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
-
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
@@ -21,9 +19,4 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
 
